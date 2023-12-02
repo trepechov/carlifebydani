3150cceacd8b81e3977ef0720c8fa8f61c210b0e
Dimitar Trepechov <trepechov@gmail.com>
2023-12-03 00:49:55 +0200

    Implement Sticky Post to Featured section

--- template-parts/front-page/featured-posts.php
+++ template-parts/front-page/featured-posts.php
@@ -1,13 +1,34 @@
+<?php
+    $args  = array(
+        'posts_per_page'      => 10,
+        'post__in'            => get_option( 'sticky_posts' ),
+        'ignore_sticky_posts' => 1,
+    );
+    $query = new WP_Query( $args );
+?>
 <div class="featured-posts">
     <div class="featured-posts-container">
-        <article class="bg-black h-80 mr-40">Hero post</article>
+        <article class="relative">
+            <?php echo get_the_post_thumbnail( $query->posts[0]->ID, array( 1024, 1024 ), array( 'class' => 'w-4/5' ) ); ?>
+            <div class="absolute right-0 top-5 p-8 bg-red-600 rounded-br-3xl w-2/5">
+                <span><?php echo date('d.m.Y', strtotime($query->posts[0]->post_date)) ?></span>
+                <span><?php echo get_the_category( $query->posts[0]->ID )[0]->name ?></span>
+                <h2 class="text-5xl"><?php echo $query->posts[0]->post_title ?></h2>
+            </div>
+        </article>
         <div>
             <h3 class="border-l-4 p-1 border-red-500">Избрано за вас<h3>
             <div class="posts-4">
-                <article class='post'>1</article>
-                <article class='post'>2</article>
-                <article class='post'>3</article>
-                <article class='post'>4</article>
+                <?php for ($i = 1; $i < 5; $i++) { 
+                    if (!empty($query->posts[$i])) {
+                ?>
+                    <article class='post'>
+                        <?php echo get_the_post_thumbnail( $query->posts[$i]->ID, array( 300, 300 ), array( 'class' => 'w-full' ) ); ?>
+                        <h3><?php echo $query->posts[$i]->post_title ?></h3>
+                        <p><?php echo date('d.m.Y', strtotime($query->posts[$i]->post_date)) ?></p>
+                    </article>
+                <?php }
+                    } ?>
             </div> 
         </div>
     </div>
