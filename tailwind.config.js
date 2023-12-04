f8a1f12305ba21062589f66c0a808d804923019b
Dimitar Trepechov <trepechov@gmail.com>
2023-12-04 23:59:45 +0200

    Add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
     content: ['./template-parts/**/*.php', './*.php', './js/*.js'],
     theme: {
-        extend: { container: { center: true } }
+        extend: {
+            container: { center: true },
+            colors: {
+                brand: {
+                    red: '#FE3652',
+                    lightgray: '#86898A',
+                    grey: '#262626',
+                    darkgrey: '#202020'
+                }
+            }
+        }
     },
     plugins: [require('@tailwindcss/typography')]
 };
