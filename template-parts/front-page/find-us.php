806b84990a71be9503ce961a20fea6601232f033
Todor Garnev <tgarnev@abv.bg>
2023-12-19 18:40:47 +0200

    added find us section

---
+++ template-parts/front-page/find-us.php
@@ -0,0 +1,57 @@
+<div class="container py-12 bg-brand-grey">
+		<div class="flex mb-8 justify-between items-center">
+    		<h3 class="border-l-8 p-3 border-brand-red uppercase">Намерете ни в</h3>
+		</div>
+
+		<div class="grid gap-8 grid-cols-2 lg:grid-cols-6">
+				<div class="social-card">
+						<a href="<?php echo get_home_url(); ?>">
+      					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/youtube.png" alt="Youtube logo" />
+      			</a>
+
+						<div class="title">@CarlifebyDani</div>
+				</div>
+
+				<div class="social-card">
+						<a href="<?php echo get_home_url(); ?>">
+      					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/patreon.png" alt="Youtube logo" />
+      			</a>
+
+						<div class="title">@carlifebydani</div>
+				</div>
+
+				<div class="social-card">
+						<a href="<?php echo get_home_url(); ?>">
+      					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/fb.png" alt="Youtube logo" />
+      			</a>
+
+						<div class="title">@carlifebydani</div>
+				</div>
+
+				<div class="social-card">
+						<a href="<?php echo get_home_url(); ?>">
+      					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/instagram.png" alt="Youtube logo" />
+      			</a>
+
+						<div class="title">@carlifebydani</div>
+				</div>
+
+				<div class="social-card">
+						<a href="<?php echo get_home_url(); ?>">
+      					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/tiktok.png" alt="Youtube logo" />
+      			</a>
+
+						<div class="title">@carlifebydani</div>
+				</div>
+
+				<div class="social-card">
+						<a href="<?php echo get_home_url(); ?>">
+      					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/spotify.png" alt="Youtube logo" />
+      			</a>
+
+						<div class="title">@CarlifebyDani</div>
+				</div>
+		</div>
+</div>
+
+</div>
