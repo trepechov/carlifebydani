5fa5c825cc19f7cccf170e5d8e2fed1f9e382e77
Dimitar Trepechov <trepechov@gmail.com>
2023-12-20 23:03:18 +0200

    Fix social media objects

--- template-parts/social-card.php
+++
@@ -1,9 +0,0 @@
-<div class="p-9 flex flex-col gap-6 items-center justify-center rounded-br-4xl cursor-pointer bg-black hover:bg-brand-solidgrey">
-		<a href="<?php echo get_permalink($args['card']->link); ?>" target="_blank">
-				<img src="<?php echo $args['card']->image; ?>" alt="Logo" />
-		</a>
-
-		<div class="text-xs font-bold text-brand-lightgrey">
-				<?php echo $args['card']->title; ?>
-		</div>
-</div>
