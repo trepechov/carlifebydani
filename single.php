ee1ff6498ab6881714e5b49bd9303985f70ae452
Dimitar Trepechov <trepechov@gmail.com>
2024-02-21 02:30:06 +0200

    Fix header title top margin

--- single.php
+++ single.php
@@ -50,7 +50,7 @@ $bread_crumbs = [
     <div class="wrapper py-8 relative">
         <?php get_template_part('template-parts/bread-crumbs', 'bread_crumbs', array('bread_crumbs' => $bread_crumbs)); ?>
 
-        <h1 class="title text-3xl/8 mb-8 font-bold <?php echo !empty($cover_image) ? 'mt-80 lg:mt-128' : 'mt-6' ?>"><?php echo $current_post->post_title; ?></h1>
+        <h1 class="title text-3xl/8 mb-8 font-bold <?php echo !empty($cover_image) ? 'mt-80 lg:mt-96' : 'mt-6' ?>"><?php echo $current_post->post_title; ?></h1>
 
         <div class="lg:grid lg:grid-cols-3 lg:gap-8">
             <div class="col-span-3">
@@ -88,7 +88,7 @@ $bread_crumbs = [
                     ?>
                 </div>
 
-                <p class="mb-4 lg:mb-0 text-[1.0625rem]"><?php echo $current_post->post_excerpt ?></p>
+                <div class="mb-4 lg:mb-0 post-content"><?php echo apply_filters('the_content', $current_post->post_excerpt); ?></div>
             </div>
             <div class="col-span-2 post-content">
                 <?php
