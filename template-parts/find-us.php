e1d75442b98d974d45d132ccfedf7202839f53aa
Dimitar Trepechov <trepechov@gmail.com>
2024-03-03 12:01:48 +0200

    Add alt props to find-us cards

--- template-parts/find-us.php
+++ template-parts/find-us.php
@@ -3,32 +3,32 @@ $socialMedias = [
 	[
 		'link' => 'https://www.youtube.com/@CarlifebyDani',
 		'image' => get_stylesheet_directory_uri() . '/images/icons/social/youtube.svg',
-		'title' => '@CarlifebyDani',
+		'title' => 'youtube',
 	],
 	[
 		'link' => 'https://instagram.com/carlifebydani',
 		'image' => get_stylesheet_directory_uri() . '/images/icons/social/instagram.svg',
-		'title' => '@carlifebydani',
+		'title' => 'instagram',
 	],
 	[
 		'link' => 'https://www.patreon.com/carlifebydani',
 		'image' => get_stylesheet_directory_uri() . '/images/icons/social/patreon.svg',
-		'title' => '@carlifebydani',
+		'title' => 'patreon',
 	],
 	[
 		'link' => 'https://open.spotify.com/show/2Bzp0LtBkArEZjV7D7V8HI',
 		'image' => get_stylesheet_directory_uri() . '/images/icons/social/spotify.svg',
-		'title' => '@CarlifebyDani',
+		'title' => 'spotify',
 	],
 	[
 		'link' => 'https://www.facebook.com/carlifebydani',
 		'image' => get_stylesheet_directory_uri() . '/images/icons/social/fb.svg',
-		'title' => '@carlifebydani',
+		'title' => 'facebook',
 	],
 	[
 		'link' => 'https://podcasts.apple.com/us/podcast/ev-masters-подкаст-за-електромобили/id1685415558',
 		'image' => get_stylesheet_directory_uri() . '/images/icons/social/apple.svg',
-		'title' => '@CarlifebyDani',
+		'title' => 'apple',
 	]
 ];
 ?>
@@ -39,7 +39,7 @@ $socialMedias = [
 
 	<div class="grid gap-8 grid-cols-2 lg:grid-cols-6">
 		<?php foreach ($socialMedias as $socialMedia) {
-			get_template_part('template-parts/card-social', 'social-card', array('social_media' => $socialMedia));
+			get_template_part('template-parts/card-social', 'social-card', ['social_media' => $socialMedia]);
 		} ?>
 	</div>
 </div>
