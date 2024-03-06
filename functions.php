a28078206b0a0e43b231da771d89001765b9f630
Dimitar Trepechov <trepechov@gmail.com>
2024-03-06 23:41:32 +0200

    Add h2 to auto tag link exeption

--- functions.php
+++ functions.php
@@ -73,7 +73,7 @@ function add_tag_links_to_content($content)
                 $tag_link = get_tag_link($tag->term_id);
                 $tag_link_html = '<a href="' . esc_url($tag_link) . '">' . esc_html($tag->name) . '</a>';
                 $content = preg_replace(
-                    '/(<((?!a|td|strong|h3|figcaption)[^>]*)>[^<]*?\b)' . preg_quote($tag->name, '/') . '(\b.*?<\/[^>]*>)/iu',
+                    '/(<((?!a|td|strong|h2|h3|figcaption)[^>]*)>[^<]*?\b)' . preg_quote($tag->name, '/') . '(\b.*?<\/[^>]*>)/iu',
                     '$1' . $tag_link_html . '$3',
                     $content,
                     5
